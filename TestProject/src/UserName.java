a443b951b51d43b23995ab557a7f637977f1e6f4
PradeepLM <56271873+PradeepLM@users.noreply.github.com>
2019-12-17 15:49:12 +0530

    all test case passed

--- TestProject/src/UserName.java
+++ TestProject/src/UserName.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+public class UserName {
 
-public class Random {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the username");
+		String name=s.next();
+		String uname="Hello username how are you ";
+		System.out.println(uname);
+		String[] array=uname.split(" ");
+		String result = null;
+		if(name.length()>3) 
+		{
+		for (int i = 0; i < array.length; i++)
+		{
+			 result=uname.replace("username",name);
+		}
+		System.out.println(result);
 	}
-
+  }
 }
+
